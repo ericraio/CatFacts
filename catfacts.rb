29a7677d82a8272475c57519c6189da8a206f377
Eric Raio <ericraio@unknown2837371b07f6.gateway.2wire.net>
2013-05-04 23:48:23 -0700

    Cat Facts

---
+++ catfacts.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'google_text'
+require 'highline/import'
+
+class CatFacts
+
+  def initialize
+    puts "What is your Google Email?"
+    username = gets.chomp
+    password = ask("What is your Google Password?\n") { |q| q.echo = false }
+    GoogleText.configure {|config| config.email, config.password = username, password}
+  end
+
+  def send_sms
+    puts "What is the number you want to send to?"
+    number = gets.chomp
+
+    puts 'What is their name?'
+    name = gets.chomp
+
+    greeting = GoogleText::Message.new(:text => "Hello #{name}, thanks for signing up to CatFacts! You will receive a free cat fact every hour!", :to => number)
+    greeting.send
+
+    loop do
+      file = File.read('catfacts').split(/\n/)
+      fact = file[Random.rand(file.size)][0..-2]
+      message = GoogleText::Message.new(:text => fact, :to => number)
+      message.send
+      puts "Sent This Cat Fact: #{fact}"
+      sleep(3600)
+    end
+  end
+
+end
+
+cf = CatFacts.new
+cf.send_sms
